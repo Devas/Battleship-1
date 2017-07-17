37e74c5761a9994c8a58fbb932a3cf2a8a5e18d9
Bartlomiej Janik <bartlomiej9519@gmail.com>
2017-07-17 22:45:46 +0200

    Another GUI upgrade: 	- ID for cells in pane that contains ships of player 	- Basic implementation of placing ships (horizontaly) 	- Colors for ships (black) 	- Distinguish if left or right mouse button was pressed

--- src/main/java/com/java_academy/battleship/gui/Controller.java
+++ src/main/java/com/java_academy/battleship/gui/Controller.java
@@ -1,32 +1,72 @@
 package sample;
 
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.*;
-import javafx.scene.paint.Color;
 
-
-import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
 public class Controller implements Initializable {
 
+    private List shipPositions;
+
+    @FXML
+    GridPane gridPane;
+
+    private void setShipPositions() {
+        Integer a[] = {0, 1, 2, 3, 99, 98, 97, 96};
+
+        shipPositions = Arrays.asList(a);
+    }
 
     public void printId(MouseEvent e) {
-        System.out.println(e.getSource());
+        setShipPositions();
+
         Pane pane = (Pane) e.getSource();
-        if ((e.getSource().toString()).equals("Pane[id=0]") || (e.getSource().toString()).equals("Pane[id=1]") || (e.getSource().toString()).equals("Pane[id=2]") || (e.getSource().toString()).equals("Pane[id=3]"))
+        int source = Integer.valueOf(e.getSource().toString().replaceAll("\\D+", ""));
+        System.out.println(source);
+        if (shipPositions.contains(source))
             pane.setStyle("-fx-background-color: red;");
         else
             pane.setStyle("-fx-background-color: cornflowerblue;");
 
+        pane.setDisable(true);
+    }
+
+    public void method(MouseEvent e){
+        System.out.println(e.getSource());
+       // if (e.getButton() == MouseButton.SECONDARY){
+
+        System.out.println(gridPane.toString());
+        int source = Integer.valueOf(e.getSource().toString().replaceAll("\\D+", ""));
+        int shipLength=3;
+        getNodeByRowColumnIndex(gridPane, source, shipLength);
+    }
+
+    public void getNodeByRowColumnIndex (GridPane gridPane, int id, int shipLength) {
+        Node result = null;
+        int counter=0;
+        List<Pane> lineList = new ArrayList<>();
+        for (Node currentNode : gridPane.getChildren()){
+            if (currentNode instanceof Pane){
+                lineList.add((Pane)currentNode);
+            }
+        }
+        for(int i=id-100; i<id+shipLength-100;++i){
+        Pane pane=lineList.get(i);
+        pane.setStyle("-fx-background-color: black;");
+        }
     }
 
     @Override
