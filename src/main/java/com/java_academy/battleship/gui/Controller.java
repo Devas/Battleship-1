4437026ff30f7d6aaec8ddeffbfab8308a810aae
Bartlomiej Janik <bartlomiej9519@gmail.com>
2017-07-18 11:17:51 +0200

    Another GUI upgrade: 	- Second GridPane is full of Panes with id 	- Ships can be placed all over second pane 	- Debugging left right click 	- ArrayList is storing ids of ships that has been planted 	- Basic algorithm to calculate where ships ids suppose to 	be(Verticly and Horizontaly)

--- src/main/java/com/java_academy/battleship/gui/Controller.java
+++ src/main/java/com/java_academy/battleship/gui/Controller.java
@@ -1,76 +1,81 @@
-package sample;
+package com.java_academy.battleship.gui;
 
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.ResourceBundle;
 
 
-public class Controller implements Initializable {
+public class Controller {
 
-    private List shipPositions;
+    private final List<Integer> shipPositions = new ArrayList<>();
 
     @FXML
     GridPane gridPane;
 
-    private void setShipPositions() {
-        Integer a[] = {0, 1, 2, 3, 99, 98, 97, 96};
-
-        shipPositions = Arrays.asList(a);
-    }
-
-    public void printId(MouseEvent e) {
-        setShipPositions();
-
+    public void handleMouseEventDuringShot(MouseEvent e) {
         Pane pane = (Pane) e.getSource();
         int source = Integer.valueOf(e.getSource().toString().replaceAll("\\D+", ""));
-        System.out.println(source);
         if (shipPositions.contains(source))
             pane.setStyle("-fx-background-color: red;");
         else
             pane.setStyle("-fx-background-color: cornflowerblue;");
 
-        pane.setDisable(true);
+        //jesli chcemy by nie mozna bylo strzelac w to miejsce dwa razy
+        // pane.setDisable(true);
     }
 
-    public void method(MouseEvent e){
-        System.out.println(e.getSource());
-       // if (e.getButton() == MouseButton.SECONDARY){
-
-        System.out.println(gridPane.toString());
-        int source = Integer.valueOf(e.getSource().toString().replaceAll("\\D+", ""));
-        int shipLength=3;
-        getNodeByRowColumnIndex(gridPane, source, shipLength);
-    }
-
-    public void getNodeByRowColumnIndex (GridPane gridPane, int id, int shipLength) {
-        Node result = null;
-        int counter=0;
+    private List<Pane> getListOfPanes() {
         List<Pane> lineList = new ArrayList<>();
-        for (Node currentNode : gridPane.getChildren()){
-            if (currentNode instanceof Pane){
-                lineList.add((Pane)currentNode);
+        for (Node currentNode : gridPane.getChildren()) {
+            if (currentNode instanceof Pane) {
+                lineList.add((Pane) currentNode);
             }
         }
-        for(int i=id-100; i<id+shipLength-100;++i){
-        Pane pane=lineList.get(i);
-        pane.setStyle("-fx-background-color: black;");
+        return lineList;
+    }
+
+    public void handleMouseClickDuringShipPlacement(MouseEvent e) {
+        int source = Integer.valueOf(e.getSource().toString().replaceAll("\\D+", ""));
+        int shipLength = 2;
+
+        List<Pane> panes = getListOfPanes();
+
+        if (e.getButton() == MouseButton.SECONDARY)
+            drawShipVertically(source, shipLength, panes);
+        else
+            drawShipHorizontaly(source, shipLength, panes);
+    }
+
+    private void drawShipVertically(int id, int shipLength, List<Pane> lineList) {
+        for (int i = id - 100; i < id + shipLength - 90; i += 10) {
+            Pane pane = lineList.get(i);
+            pane.setStyle("-fx-background-color: black;");
+            shipPositions.add(i);
+        }
+
+    }
+
+    private void drawShipHorizontaly(int id, int shipLength, List<Pane> lineList) {
+        for (int i = id - 100; i < id + shipLength - 100; ++i) {
+            Pane pane = lineList.get(i);
+            pane.setStyle("-fx-background-color: black;");
+            shipPositions.add(i);
         }
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
+    public void placeShipsRandomly() {
+        System.out.println("Random ships");
     }
+
+    public void deployNuke() {
+        System.out.println("Deploy nuke");
+    }
+
 }
--- src/main/java/com/java_academy/battleship/gui/Main.java
+++ src/main/java/com/java_academy/battleship/gui/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.java_academy.battleship.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,11 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        URI url=new File("src/main/java/com/java_academy/battleship/gui/sample.fxml").toURI();
+        Parent root = FXMLLoader.load(url.toURL());
         primaryStage.setTitle("Battleships");
         primaryStage.setScene(new Scene(root, 950, 700));
         primaryStage.show();
